34d60d7d799ce405482d7c084919b1230ebc161d
Duke984 <miguelgiradu@gmail.com>
2021-06-07 16:44:09 -0500

    Creacion y maquetacion del css y html

---
+++ oas.py
@@ -0,0 +1,13 @@
+import random
+
+areas = ["Finansas","ingles","matematicas","programacion"]
+modulos = [1,2,3]
+
+aleatorio_areas =random.sample(areas,3)
+
+for area in aleatorio_areas:
+    aleatorio_modulos = random.choice(modulos)
+    print (aleatorio_areas + " " +(str aleatorio_modulos ))
+
+
+
